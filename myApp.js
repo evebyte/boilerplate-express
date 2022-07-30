e726dc096cb01e749fb1d0c35fa867fe6a0f7a56
eve <49453037+evebyte@users.noreply.github.com>
2022-07-30 07:19:53 -0600

    working through challenges + learning to use middleware

--- myApp.js
+++ myApp.js
@@ -1,14 +1,46 @@
+// imports express and creates an express app
 require("dotenv").config();
 let express = require("express");
 let app = express();
 
-// console.log("Hello World!");
+// add simple logger for all requests
+app.use((req, res, next) => {
+	console.log(`${req.method} ${req.path} - ${req.ip}`);
+	next();
+});
 
+// root route sends user index.html
 app.get("/", (req, res) => {
 	// res.send("Hello Express");
 	res.sendFile(__dirname + "/views/index.html");
 });
 
+// we can chain a middleware function and the final handler together
+// adds the current time to the request object
+app.get(
+	"/now",
+	(req, res, next) => {
+		res.json({
+			time: new Date().toString(),
+		});
+		next();
+	},
+	(req, res) => {
+		console.log("/now route");
+	}
+);
+
+// echos the request word back in json
+app.get("/:word/echo", (req, res) => {
+	// get the word from the request
+	const word = req.params.word;
+
+	res.json({
+		echo: req.params.word,
+	});
+});
+
+// json route sends json data back
 app.get("/json", (req, res) => {
 	if (process.env.MESSAGE_STYLE === "uppercase") {
 		res.json({ message: "Hello json".toUpperCase() });
@@ -17,6 +49,7 @@ app.get("/json", (req, res) => {
 	}
 });
 
+// mounts middleware to serve static files
 app.use("/public", express.static(__dirname + "/public"));
 
 module.exports = app;
