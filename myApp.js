e2782e17dad30cce0796473f45f3007834b219bd
eve <49453037+evebyte@users.noreply.github.com>
2022-07-30 02:15:15 -0600

    learning to make api's with express and node :)

--- myApp.js
+++ myApp.js
@@ -1,6 +1,22 @@
+require("dotenv").config();
 let express = require("express");
 let app = express();
 
-console.log("Hello World!");
+// console.log("Hello World!");
+
+app.get("/", (req, res) => {
+	// res.send("Hello Express");
+	res.sendFile(__dirname + "/views/index.html");
+});
+
+app.get("/json", (req, res) => {
+	if (process.env.MESSAGE_STYLE === "uppercase") {
+		res.json({ message: "Hello json".toUpperCase() });
+	} else {
+		res.json({ message: "Hello json" });
+	}
+});
+
+app.use("/public", express.static(__dirname + "/public"));
 
 module.exports = app;
